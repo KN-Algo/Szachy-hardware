d656848eeaf582db4bf4ac26d10d536a20da03a3
Igor Olewicz <94185939+CZIWELLO@users.noreply.github.com>
2025-10-15 22:31:42 +0200

    qf

--- Kod/Rasberry_pi_API/square_to_cords.py
+++ Kod/Rasberry_pi_API/square_to_cords.py
@@ -1,9 +1,9 @@
-from config import SQUARE_SIZE, BOARD_ORIGIN_X_MM, BOARD_ORIGIN_Y_MM
+from config import BOARD_ORIGIN_X_MM, BOARD_ORIGIN_Y_MM, SQUARE_SIZE
 
 
 def square_to_coords(square):
     file = square[0].lower()  # a-h
     rank = square[1]  # 1-8
-    x = (ord(file) - ord("a")) * SQUARE_SIZE + BOARD_ORIGIN_X_MM
-    y = (int(rank) - 1) * SQUARE_SIZE + BOARD_ORIGIN_Y_MM
+    x = round((ord(file) - ord("a")) * SQUARE_SIZE + BOARD_ORIGIN_X_MM, 2)
+    y = round((int(rank) - 1) * SQUARE_SIZE + BOARD_ORIGIN_Y_MM, 2)
     return x, y
